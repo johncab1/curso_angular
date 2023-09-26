f1537c25b6cd5ddcc967929ab850fbb5d9e3a130
Johncab1 <jonatan.orc@gmail.com>
2023-09-26 15:19:28 -0600

    interpolación

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-contador',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./contador.component.css']
 })
 export class ContadorComponent {
+  constructor(){}
 
+  ngOnInit(): void{
+
+  }
+
+  //nombre:string = "Jonatan Rodríguez";
+
+  persona:Persona = {
+    nombre: 'Jonatan Rodríguez',
+    edad: 30
+  }
 }
