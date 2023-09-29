8f64c7e9686a8f292a7c46ea4003f5158b1a5ff1
Johncab1 <jonatan.orc@gmail.com>
2023-09-28 22:45:00 -0600

    binding (cambio dinámico de  clases en botones y texto)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { BotonesComponent } from './botones/botones.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
+    BotonesComponent,
 
   ],
   imports: [
