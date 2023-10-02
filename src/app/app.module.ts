227c67c9e13b21f494cc04d797059f4886f80182
Johncab1 <jonatan.orc@gmail.com>
2023-10-01 19:23:36 -0600

    variables de plantilla (con formularios)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { BotonesComponent } from './botones/botones.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { BotonesComponent } from './botones/botones.component';
     AppComponent,
     ContadorComponent,
     BotonesComponent,
+    FormularioComponent,
 
   ],
   imports: [
