0f3432da66f185c8fde25b378637d04948ac5dde
Johncab1 <jonatan.orc@gmail.com>
2023-10-01 19:05:53 -0600

    property binding

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BotonesComponent {
 
-  textColor:string = "";
-  buttonDisabled:boolean = false;
+/*   textColor:string = "";
+  buttonDisabled:boolean = false; */
+
+  src:string = "https://images.pexels.com/photos/14060070/pexels-photo-14060070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"; 
 }
