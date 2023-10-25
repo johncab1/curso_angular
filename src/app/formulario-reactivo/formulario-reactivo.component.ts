cb93efd52e84440f3775d15fc8fb2f1b60a89f56
Johncab1 <jonatan.orc@gmail.com>
2023-10-24 21:11:08 -0600

    formularios reactivos - validators

--- src/app/formulario-reactivo/formulario-reactivo.component.ts
+++ src/app/formulario-reactivo/formulario-reactivo.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-formulario-reactivo',
   templateUrl: './formulario-reactivo.component.html',
   styleUrls: ['./formulario-reactivo.component.css']
 })
 export class FormularioReactivoComponent {
-name = new FormControl('');
-email = new FormControl('');
+name = new FormControl('', Validators.required);
+email = new FormControl('', [Validators.required, Validators.email]);
 }
