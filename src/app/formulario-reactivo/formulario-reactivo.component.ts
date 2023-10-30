24c54a52305da10e7332c4725cc5ba8751624733
Johncab1 <jonatan.orc@gmail.com>
2023-10-29 18:53:47 -0600

    Formularios reactivos - formGroup

--- src/app/formulario-reactivo/formulario-reactivo.component.ts
+++ src/app/formulario-reactivo/formulario-reactivo.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-formulario-reactivo',
   templateUrl: './formulario-reactivo.component.html',
   styleUrls: ['./formulario-reactivo.component.css']
 })
 export class FormularioReactivoComponent {
-name = new FormControl('', Validators.required);
-email = new FormControl('', [Validators.required, Validators.email]);
+
+get name()
+{
+  return this.formUser.get('name') as FormControl;
+}
+
+get email()
+{
+  return this.formUser.get('email') as FormControl;
+}
+
+formUser = new FormGroup({
+  'name': new FormControl('', Validators.required),
+'email': new FormControl('', [Validators.required, Validators.email])
+});
+
+procesar()
+{
+  console.log(this.formUser.value);
+}
+
+/* name = new FormControl('', Validators.required);
+email = new FormControl('', [Validators.required, Validators.email]); */
 }
